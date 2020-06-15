36df2f530a784437af4e2640983f7f83e88fab2d
DavidDasilva911 <55771963+DavidDasilva911@users.noreply.github.com>
2020-06-15 11:00:53 +0700

    tugas3 pwpb
    1

---
+++ tugas3 pwpb.php
@@ -0,0 +1,8 @@
+<?php
+    echo' Nama : Mochammad David Dasilva Akbar <br>';
+    echo' Kelas : XI Rpl 1 <br>';
+    echo "SMK Taruna Bhakti";
+    echo "Jatijajar RT04 RW12";
+    echo' Email : davidchibulan@gmail.com<br>' ;
+    echo' No Telepon : 0895402180101<br>';
+    ?>
